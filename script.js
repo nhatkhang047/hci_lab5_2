8abe09dbf43b2d768d82f84cae1f1b7aeb6fd8b8
Nhat Khang <phamnhatkhang420@gmail.com>
2024-11-01 23:09:51 +0700

    lab5_2

---
+++ script.js
@@ -0,0 +1,24 @@
+// Hàm JavaScript để chuyển đến trang Learning Paths
+function goToLearningPaths() {
+  // Chuyển trang đến URL của trang Learning Paths
+  window.location.href = "index/a.html";
+}
+function goToAllCourses() {
+  window.location.href = "index/all-courses.html";
+}
+
+function goToLearningPaths() {
+  window.location.href = "index/a.html";
+}
+
+function goToCertifications() {
+  window.location.href = "index/certifications.html";
+}
+
+function goToAchieved() {
+  window.location.href = "index/c.html";
+}
+
+function goToLearningTools() {
+  window.location.href = "index/d.html";
+}
